e14620c6da0837e20c010e5e449708b754cb6bf5
d4yzz <dayzz.george@gmail.com>
2025-04-06 16:37:17 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet(''))("")
-loadstring(game:HttpGet(''))("")
-loadstring(game:HttpGet(''))("")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/d4yzz/Atomic-Hub/refs/heads/main/events.lua'))("") -- Events Loader
+loadstring(game:HttpGet('https://raw.githubusercontent.com/d4yzz/Atomic-Hub/refs/heads/main/gui.lua'))("") -- GUI Loader
+loadstring(game:HttpGet('https://raw.githubusercontent.com/d4yzz/Atomic-Hub/refs/heads/main/serverscripts.lua'))("") -- Server Side Scripts Loader
