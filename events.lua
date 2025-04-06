b5ad76b9fbd072aa1f833100d13a4d36c8d07347
d4yzz <dayzz.george@gmail.com>
2025-04-06 16:33:26 +0100

    Create events.lua

---
+++ events.lua
@@ -0,0 +1,10 @@
+-- Events
+local DataTransferEvent = Instance.new("RemoteEvent")
+DataTransferEvent.Name = "DataTransferEvent"
+DataTransferEvent.Parent = game.ReplicatedStorage
+local HttpRequestEvent = Instance.new("RemoteEvent")
+HttpRequestEvent.Name = "HttpRequestEvent"
+HttpRequestEvent.Parent = game.ReplicatedStorage
+local WebhookDataEvent = Instance.new("RemoteEvent")
+WebhookDataEvent.Name = "WebhookDataEvent"
+WebhookDataEvent.Parent = game.ReplicatedStorage
